41083269f4ec92e29f51c0dba2efda9fdc343973
MuchiriKinyua <muchirikinyua2000@gmail.com>
2025-03-01 15:40:35 +0300

    blog images

--- app/Http/Controllers/blogFrController.php
+++ app/Http/Controllers/blogFrController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\blogs;
-use App\Models\setting;
+use App\Models\Blogs;
+use App\Models\Setting;
 
 class blogFrController extends Controller
 {
     public function index()
     {
-        $blogs = blogs::all();
+        $blogs = Blogs::all();
         $settings = Setting::all();
         return view('school.blog', compact('blogs', 'settings'));
     }
